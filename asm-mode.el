9f64bdb58ac9c8e07ef132061e061bca01baeb6c
Tuomo Venäläinen <tuomoster@gmail.com>
2017-08-27 23:44:49 +0300

    better README + other fixes

--- asm-mode.el
+++ asm-mode.el
@@ -1,8 +1,8 @@
 ;; -*-no-byte-compile: t; -*-
 
 (defun vendetta-asm-mode-hook()
-  (setq indent-tabs-mode nil)
   (setq tab-always-indent nil)
+  (setq indent-tabs-mode nil)
   (setq indent-line-function 'insert-tab)
   (local-unset-key (vector asm-comment-char)))
 
--- emacs-lisp-mode.el
+++ emacs-lisp-mode.el
@@ -2,10 +2,6 @@
 
 ;; emacs lisp functionality
 
-(custom-theme-set-variables
- 'vendetta
- '(indent-tabs-mode nil))
-
 ;(defun vendetta-emacs-lisp-after-save-hook()
 ;  (let* file-extension (file-name-extension buffer-file-name))
 ;  ((if (or (string-equal (downcase file-extension) "elc")
@@ -20,6 +16,7 @@
 
 (defun vendetta-emacs-lisp-mode-hook()
   ;;	    (define-key flyspell-mode-map "\M-\t" nil)
+  (setq indent-tabs-mode nil)
   (define-key emacs-lisp-mode-map "\C-x\C-e" 'pp-eval-last-sexp)
   (define-key emacs-lisp-mode-map [ret] 'reindent-then-newline-and-indent))
 
